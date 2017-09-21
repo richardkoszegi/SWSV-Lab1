8842f30c882c83bc610bb3d29ec9f9c9b64ec6ba
Richard Koszegi <richard.koszegi95@gmail.com>
2017-09-21 14:52:03 +0200

    initial commit

---
+++ src/main/java/com/cur454/app/App.java
@@ -0,0 +1,13 @@
+package com.cur454.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
